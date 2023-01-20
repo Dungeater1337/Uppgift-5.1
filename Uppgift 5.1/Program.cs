743f861718a3601324a1aeda6b0634ed4e13817d
Meysam03 <simon.munter@icloud.com>
2023-01-20 10:07:55 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+namespace uppgift5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Hur mår du?", "Godmorgon hur mår du?", "Jag har nyss tvättat bilen.", "Jag ska handla på ICA efter jobbet." };
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] +"\n");    
+            }
+        }
+    }
+}
